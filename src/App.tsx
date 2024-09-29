1719c1ba10708b90d208958e70a64adfb71669fa
EvgeniiNoName <125493179+EvgeniiNoName@users.noreply.github.com>
2024-09-29 20:58:19 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import './App.css'
+import './App.css';
 import { Listing } from './components/Listing';
 import json from '../public/data/etsy.json';
+import { IItem } from './models';
+
+const items: IItem[] = json as IItem[];
 
 function App() {
-  const items: Array<any> = json.filter(
-    item => item.listing_id &&
+  const filteredItems = items.filter(item =>
+    item.listing_id &&
     item.title &&
     item.currency_code &&
     item.price &&
-    item.quantity
+    item.quantity &&
+    item.MainImage?.url_570xN
   );
 
   return (
-    <Listing items = {items} />
+    <Listing items={filteredItems} />
   );
 }
 
-export default App
+export default App;
